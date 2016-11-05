ffa5a4a8adbd54d248d20c87ca7db909e33ae0f4
KHACHIAI Oussama <oussama.khachiai@gmail.com>
2016-11-05 03:05:25 +0000

    the landing page

--- src/Geekdoos/FrontBundle/Controller/DefaultController.php
+++ src/Geekdoos/FrontBundle/Controller/DefaultController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    public function landingAction()
+    {
+        return $this->render('GeekdoosFrontBundle:Default:landing.html.twig');
+    }
+
     public function indexAction()
     {
         return $this->render('GeekdoosFrontBundle:Default:index.html.twig');
--- src/Geekdoos/UserBundle/Controller/DefaultController.php
+++ src/Geekdoos/UserBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('GeekdoosUserBundle:Default:index.html.twig');
+        return $this->render('@GeekdoosFront/Default/index.html.twig');
     }
 }
